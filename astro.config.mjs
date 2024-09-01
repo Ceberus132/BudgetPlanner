5f01b72a9daf92df026513c8f39ad750616eaf8a
Ceberus132 <ceberus132@gmail.com>
2024-09-01 14:55:38 +0200

    feat: added cloudflare adapter 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import db from '@astrojs/db';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), db()]
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [tailwind(), react(), db()],
 });
