053f3ed552f2b9e2b0bc7d58525cad905c71660b
Ceberus132 <ceberus132@gmail.com>
2024-09-01 08:38:48 +0200

    feat: added Transaction Table 🔥

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,20 @@
-import { defineDb } from 'astro:db';
+import {column, defineDb, defineTable} from 'astro:db';
+
+const Transaction = defineTable({
+  columns: {
+    source: column.text(),
+    amount: column.number(),
+    type: column.text(),
+    date: column.date(),
+    recurring: column.boolean(),
+    frequency: column.text(),
+    interval: column.number(),
+    dayOfWeek: column.number(),
+    endDate: column.date()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Transaction }
 });
