0da3c68f68107135a5baf67aa85bd8c796457e86
Ceberus132 <ceberus132@gmail.com>
2024-09-03 13:58:14 +0200

    feat: initial calendar component

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+export default function Calendar() {
+
+    return(
+        <div className={''}></div>
+    )
+}
