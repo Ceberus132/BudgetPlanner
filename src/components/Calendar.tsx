c82847cd312e6fa098068887ac3c8cdfef9d25fe
Ceberus132 <ceberus132@gmail.com>
2024-09-03 14:01:36 +0200

    feat: added func to get the days in a month

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
-const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const getMonthDays = (year: number, month: number) => {
+    return new Date(year, month + 1, 0).getDate();
+};
 
 export default function Calendar() {
 
